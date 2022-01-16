e146a62ad943dbd4b3224946ca549b2b3e7f77cb
oleksandr.khomenko <oleksandr.khomenko@leverx.com>
2022-01-16 21:22:32 +0200

    1

---
+++ src/ormconfig.ts
@@ -0,0 +1,11 @@
+import { ConnectionOptions } from 'typeorm';
+
+const config: ConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'test',
+};
+export default config;
